fc80fffe2e15c317d77cd2c8e8eeaff12bd39edd
m03951659gmail <m03951659@gmail.com>
2025-11-02 21:23:04 +0600

    Create script.js

---
+++ tool-1/Fake Tweet generator 32/script.js
@@ -0,0 +1,393 @@
+function _0x77b9(_0x2dc063, _0xdcaa82) {
+    const _0x234d09 = _0x234d();
+    return _0x77b9 = function (_0x77b996, _0x37d127) {
+        _0x77b996 = _0x77b996 - 0x1b3;
+        let _0x1a879e = _0x234d09[_0x77b996];
+        return _0x1a879e;
+    }, _0x77b9(_0x2dc063, _0xdcaa82);
+}
+const _0x3618b2 = _0x77b9;
+(function (_0x42eadd, _0x3bffc6) {
+    const _0x266551 = _0x77b9, _0x890705 = _0x42eadd();
+    while (!![]) {
+        try {
+            const _0x19e532 = -parseInt(_0x266551(0x1f5)) / 0x1 + -parseInt(_0x266551(0x1df)) / 0x2 * (parseInt(_0x266551(0x1dd)) / 0x3) + parseInt(_0x266551(0x1f8)) / 0x4 * (parseInt(_0x266551(0x1e5)) / 0x5) + parseInt(_0x266551(0x1b5)) / 0x6 + -parseInt(_0x266551(0x1d9)) / 0x7 + parseInt(_0x266551(0x1d4)) / 0x8 + parseInt(_0x266551(0x1f6)) / 0x9;
+            if (_0x19e532 === _0x3bffc6)
+                break;
+            else
+                _0x890705['push'](_0x890705['shift']());
+        } catch (_0x36b671) {
+            _0x890705['push'](_0x890705['shift']());
+        }
+    }
+}(_0x234d, 0xc3047), document['addEventListener'](_0x3618b2(0x1c6), function () {
+    const _0x1f9ee6 = _0x3618b2, _0x1badeb = document['getElementById'](_0x1f9ee6(0x1f2)), _0x62335b = document[_0x1f9ee6(0x1f7)]('display-name'), _0x2e949a = document[_0x1f9ee6(0x1f7)]('username'), _0x5a3d2e = document[_0x1f9ee6(0x1f7)]('verified'), _0x47907c = document[_0x1f9ee6(0x1f7)](_0x1f9ee6(0x200)), _0x3a2e23 = document[_0x1f9ee6(0x1f7)](_0x1f9ee6(0x1d6)), _0x2eb820 = document['getElementById'](_0x1f9ee6(0x1c1)), _0x4a9b32 = document['getElementById']('retweets'), _0x48b248 = document[_0x1f9ee6(0x1f7)]('likes'), _0x1a457d = document[_0x1f9ee6(0x1f7)]('generate-tweet'), _0x208800 = document[_0x1f9ee6(0x1f7)](_0x1f9ee6(0x1b9)), _0x2a958c = document['getElementById'](_0x1f9ee6(0x1ba)), _0x1f4173 = document[_0x1f9ee6(0x1f7)]('add-emoji'), _0x5917fb = document[_0x1f9ee6(0x1f7)](_0x1f9ee6(0x1d1)), _0x57408c = document[_0x1f9ee6(0x1f7)](_0x1f9ee6(0x1c0)), _0x4ee6e6 = document[_0x1f9ee6(0x1f7)](_0x1f9ee6(0x201)), _0x347f0b = new Date(), _0x1dfebf = _0x347f0b['getFullYear'](), _0x10b739 = String(_0x347f0b[_0x1f9ee6(0x1d7)]() + 0x1)[_0x1f9ee6(0x205)](0x2, '0'), _0x385919 = String(_0x347f0b['getDate']())['padStart'](0x2, '0'), _0x142729 = String(_0x347f0b[_0x1f9ee6(0x1f9)]())[_0x1f9ee6(0x205)](0x2, '0'), _0x1626ad = String(_0x347f0b[_0x1f9ee6(0x1be)]())[_0x1f9ee6(0x205)](0x2, '0');
+    _0x3a2e23['value'] = _0x1dfebf + '-' + _0x10b739 + '-' + _0x385919 + 'T' + _0x142729 + ':' + _0x1626ad;
+    const _0x5cf113 = [
+        '😀',
+        '😃',
+        '😄',
+        '😁',
+        '😆',
+        '😅',
+        '😂',
+        '🤣',
+        '😊',
+        '😇',
+        '🙂',
+        '🙃',
+        '😉',
+        '😌',
+        '😍',
+        '🥰',
+        '😘',
+        '😗',
+        '😙',
+        '😚',
+        '😋',
+        '😛',
+        '😝',
+        '😜',
+        '🤪',
+        '🤨',
+        '🧐',
+        '🤓',
+        '😎',
+        '🤩',
+        '🥳',
+        '😏',
+        '😒',
+        '😞',
+        '😔',
+        '😟',
+        '😕',
+        '🙁',
+        '☹️',
+        '😣',
+        '😖',
+        '😫',
+        '😩',
+        '🥺',
+        '😢',
+        '😭',
+        '😤',
+        '😠',
+        '😡',
+        '🤬',
+        '🤯',
+        '😳',
+        '🥵',
+        '🥶',
+        '😱',
+        '😨',
+        '😰',
+        '😥',
+        '😓',
+        '🤗',
+        '🤔',
+        '🤭',
+        '🤫',
+        '🤥',
+        '😶',
+        '😐',
+        '😑',
+        '😬',
+        '🙄',
+        '😯',
+        '😦',
+        '😧',
+        '😮',
+        '😲',
+        '🥱',
+        '😴',
+        '🤤',
+        '😪',
+        '😵',
+        '🤐',
+        '🥴',
+        '🤢',
+        '🤮',
+        '🤧',
+        '😷',
+        '🤒',
+        '🤕',
+        '🤑',
+        '🤠',
+        '😈',
+        '👿',
+        '👹',
+        '👺',
+        '🤡',
+        '💩',
+        '👻',
+        '💀',
+        '☠️',
+        '👽',
+        '👾',
+        '🤖',
+        '🎃',
+        '😺',
+        '😸',
+        '😹',
+        '😻',
+        '😼',
+        '😽',
+        '🙀',
+        '😿',
+        '😾',
+        '👋',
+        '🤚',
+        _0x1f9ee6(0x1bf),
+        '✋',
+        '🖖',
+        '👌',
+        '🤏',
+        '✌️',
+        '🤞',
+        '🤟',
+        '🤘',
+        '🤙',
+        '👈',
+        '👉',
+        '👆',
+        '🖕',
+        '👇',
+        '☝️',
+        '👍',
+        '👎',
+        '✊',
+        '👊',
+        '🤛',
+        '🤜',
+        '👏',
+        '🙌',
+        '👐',
+        '🤲',
+        '🤝',
+        '🙏',
+        '✍️',
+        '💅',
+        '🤳',
+        '💪',
+        '🦾',
+        '🦿',
+        '🦵',
+        '🦶',
+        '👂',
+        '🦻',
+        '👃',
+        '🧠',
+        '🦷',
+        '🦴',
+        '👀',
+        _0x1f9ee6(0x1cb),
+        '👅',
+        '👄',
+        '💋',
+        '🩸',
+        '💘',
+        '💝',
+        '💖',
+        '💗',
+        '💓',
+        '💞',
+        '💕',
+        '💟',
+        '❣️',
+        '💔',
+        '❤️',
+        '🧡',
+        '💛',
+        '💚',
+        '💙',
+        '💜',
+        '🤎',
+        '🖤',
+        '🤍',
+        '💯',
+        '💢',
+        '💥',
+        '💫',
+        '💦',
+        '💨',
+        _0x1f9ee6(0x1f3),
+        '💣',
+        '💬',
+        _0x1f9ee6(0x1ce),
+        '🗨️',
+        _0x1f9ee6(0x1fd),
+        '💭',
+        '💤'
+    ];
+    _0x5cf113['forEach'](_0x594742 => {
+        const _0x116656 = _0x1f9ee6, _0x4660ef = document[_0x116656(0x1c4)](_0x116656(0x1ed));
+        _0x4660ef[_0x116656(0x1e3)] = _0x116656(0x1d5), _0x4660ef[_0x116656(0x1bb)] = _0x594742, _0x4660ef['addEventListener'](_0x116656(0x1d2), () => {
+            const _0x8fb857 = _0x116656;
+            _0x20a0a4(_0x594742), _0x5917fb[_0x8fb857(0x1fe)][_0x8fb857(0x1c2)](_0x8fb857(0x1ee));
+        }), _0x4ee6e6[_0x116656(0x1f4)](_0x4660ef);
+    }), _0x1a457d[_0x1f9ee6(0x1e2)](_0x1f9ee6(0x1d2), _0x471921), _0x208800[_0x1f9ee6(0x1e2)]('click', _0x3f3115), _0x1f4173['addEventListener'](_0x1f9ee6(0x1d2), () => _0x5917fb['classList'][_0x1f9ee6(0x206)](_0x1f9ee6(0x1ee))), _0x57408c[_0x1f9ee6(0x1e2)](_0x1f9ee6(0x1d2), () => _0x5917fb['classList'][_0x1f9ee6(0x1c2)]('hidden')), _0x1badeb[_0x1f9ee6(0x1e2)](_0x1f9ee6(0x1ec), _0x24ce6e), _0x471921();
+    function _0x24ce6e(_0xafa13b) {
+        const _0x160f2a = _0x1f9ee6, _0x1c44d4 = _0xafa13b['target'][_0x160f2a(0x1b6)][0x0];
+        if (_0x1c44d4) {
+            const _0x3ad532 = new FileReader();
+            _0x3ad532[_0x160f2a(0x1fc)] = function (_0x315447) {
+                _0x471921();
+            }, _0x3ad532[_0x160f2a(0x204)](_0x1c44d4);
+        }
+    }
+    function _0x20a0a4(_0x5a56f6) {
+        const _0x4b165e = _0x1f9ee6, _0x424c69 = _0x47907c, _0x44e1c5 = _0x424c69[_0x4b165e(0x1dc)], _0x3a160a = _0x424c69[_0x4b165e(0x1c8)], _0x32e236 = _0x424c69[_0x4b165e(0x1c5)];
+        _0x424c69['value'] = _0x32e236[_0x4b165e(0x1e7)](0x0, _0x44e1c5) + _0x5a56f6 + _0x32e236[_0x4b165e(0x1e7)](_0x3a160a), _0x424c69[_0x4b165e(0x1dc)] = _0x424c69[_0x4b165e(0x1c8)] = _0x44e1c5 + _0x5a56f6[_0x4b165e(0x1da)], _0x424c69[_0x4b165e(0x1db)]();
+    }
+    function _0x471921() {
+        const _0x46070b = _0x1f9ee6, _0x483b21 = _0x62335b[_0x46070b(0x1c5)] || _0x46070b(0x1cc), _0x597df1 = _0x2e949a['value'] || 'twitteruser', _0x346a4f = _0x5a3d2e[_0x46070b(0x202)], _0x17cd55 = _0x47907c[_0x46070b(0x1c5)] || _0x46070b(0x1bc), _0x1000ce = new Date(_0x3a2e23[_0x46070b(0x1c5)] || new Date()), _0x6c73a0 = _0x2eb820[_0x46070b(0x1c5)] || '0', _0x3a9313 = _0x4a9b32[_0x46070b(0x1c5)] || '0', _0x1263de = _0x48b248['value'] || '0', _0x34de8f = _0x3dd044(_0x1000ce);
+        let _0x3a808d = '\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22tweet\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22tweet-header\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20' + _0x43b69e() + _0x46070b(0x1c9) + _0x590a28(_0x483b21) + _0x46070b(0x1b8) + (_0x346a4f ? _0x46070b(0x1eb) : '') + '\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<span\x20class=\x22tweet-username\x22>@' + _0x590a28(_0x597df1) + _0x46070b(0x207) + _0x1dbd80(_0x17cd55) + '</div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22tweet-date\x22>' + _0x34de8f + _0x46070b(0x1c7) + _0x6c73a0 + '</span>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22tweet-stat\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<svg\x20viewBox=\x220\x200\x2024\x2024\x22><g><path\x20d=\x22M23.77\x2015.67c-.292-.293-.767-.293-1.06\x200l-2.22\x202.22V7.65c0-2.068-1.683-3.75-3.75-3.75h-5.85c-.414\x200-.75.336-.75.75s.336.75.75.75h5.85c1.24\x200\x202.25\x201.01\x202.25\x202.25v10.24l-2.22-2.22c-.293-.293-.768-.293-1.06\x200s-.294.768\x200\x201.06l3.5\x203.5c.145.147.337.22.53.22s.383-.072.53-.22l3.5-3.5c.294-.292.294-.767\x200-1.06zm-10.66\x203.28H7.26c-1.24\x200-2.25-1.01-2.25-2.25V6.46l2.22\x202.22c.148.147.34.22.532.22s.384-.073.53-.22c.293-.293.293-.768\x200-1.06l-3.5-3.5c-.293-.294-.768-.294-1.06\x200l-3.5\x203.5c-.294.292-.294.767\x200\x201.06s.767.293\x201.06\x200l2.22-2.22V16.7c0\x202.068\x201.683\x203.75\x203.75\x203.75h5.85c.414\x200\x20.75-.336.75-.75s-.337-.75-.75-.75z\x22></path></g></svg>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<span>' + _0x3a9313 + _0x46070b(0x209) + _0x1263de + _0x46070b(0x1e4);
+        _0x2a958c[_0x46070b(0x1fb)] = _0x3a808d;
+    }
+    function _0x43b69e() {
+        const _0x3982e9 = _0x1f9ee6;
+        if (_0x1badeb[_0x3982e9(0x1b6)] && _0x1badeb[_0x3982e9(0x1b6)][0x0]) {
+            const _0x3f916a = new FileReader();
+            return _0x3f916a[_0x3982e9(0x1fc)] = function (_0xc44e8d) {
+                const _0x1ac84f = _0x3982e9;
+                document['querySelector'](_0x1ac84f(0x1f0))['src'] = _0xc44e8d[_0x1ac84f(0x1f1)]['result'];
+            }, _0x3f916a['readAsDataURL'](_0x1badeb[_0x3982e9(0x1b6)][0x0]), _0x3982e9(0x20a);
+        }
+        return _0x3982e9(0x1de);
+    }
+    function _0x3dd044(_0xcad7ea) {
+        const _0x204655 = _0x1f9ee6, _0x5c4d11 = {
+                'hour': _0x204655(0x1e9),
+                'minute': _0x204655(0x203),
+                'hour12': !![]
+            }, _0xae6d66 = _0xcad7ea[_0x204655(0x1d0)](_0x204655(0x1b7), _0x5c4d11), _0x5656f9 = {
+                'month': 'short',
+                'day': _0x204655(0x1e9),
+                'year': _0x204655(0x1e9)
+            }, _0x263d8e = _0xcad7ea[_0x204655(0x1fa)](_0x204655(0x1b7), _0x5656f9);
+        return _0xae6d66 + '\x20·\x20' + _0x263d8e;
+    }
+    function _0x1dbd80(_0x15a4d4) {
+        const _0x21e192 = _0x1f9ee6;
+        return _0x590a28(_0x15a4d4)[_0x21e192(0x1e8)](/\n/g, _0x21e192(0x1e1));
+    }
+    function _0x590a28(_0xda49b3) {
+        const _0x2dbb58 = _0x1f9ee6;
+        return _0xda49b3[_0x2dbb58(0x1e8)](/&/g, _0x2dbb58(0x1ea))['replace'](/</g, '&lt;')[_0x2dbb58(0x1e8)](/>/g, _0x2dbb58(0x208))['replace'](/"/g, _0x2dbb58(0x1bd))[_0x2dbb58(0x1e8)](/'/g, _0x2dbb58(0x1cf));
+    }
+    function _0x3f3115() {
+        const _0x5c6c68 = _0x1f9ee6;
+        html2canvas(_0x2a958c, {
+            'backgroundColor': _0x5c6c68(0x1ca),
+            'scale': 0x2
+        })[_0x5c6c68(0x1cd)](_0x9a71c9 => {
+            const _0x245e17 = _0x5c6c68, _0x5c5575 = document[_0x245e17(0x1c4)]('a');
+            _0x5c5575[_0x245e17(0x1e0)] = _0x245e17(0x1d8), _0x5c5575[_0x245e17(0x1b3)] = _0x9a71c9['toDataURL']('image/png'), _0x5c5575['click']();
+        });
+    }
+}));
+const allowedDomains = [
+        _0x3618b2(0x1d3),
+        'localhost',
+        _0x3618b2(0x1e6)
+    ], currentHost = window['location'][_0x3618b2(0x1b4)];
+if (!allowedDomains[_0x3618b2(0x1ef)](currentHost)) {
+    document[_0x3618b2(0x1ff)][_0x3618b2(0x1fb)] = _0x3618b2(0x1c3);
+    throw new Error('This\x20game\x20is\x20not\x20allowed\x20to\x20run\x20on\x20this\x20domain!');
+}
+function _0x234d() {
+    const _0x436676 = [
+        '&gt;',
+        '</span>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22tweet-stat\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<svg\x20viewBox=\x220\x200\x2024\x2024\x22><g><path\x20d=\x22M12\x2021.638h-.014C9.403\x2021.59\x201.95\x2014.856\x201.95\x208.478c0-3.064\x202.525-5.754\x205.403-5.754\x202.29\x200\x203.83\x201.58\x204.646\x202.73.814-1.148\x202.354-2.73\x204.645-2.73\x202.88\x200\x205.404\x202.69\x205.404\x205.755\x200\x206.376-7.454\x2013.11-10.037\x2013.157H12z\x22></path></g></svg>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<span>',
+        '<img\x20class=\x22tweet-avatar\x22\x20src=\x22\x22\x20alt=\x22Profile\x20picture\x22>',
+        'href',
+        'hostname',
+        '4138992zKxAiD',
+        'files',
+        'en-US',
+        '</span>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20',
+        'download-tweet',
+        'tweet-preview',
+        'textContent',
+        'This\x20is\x20a\x20sample\x20tweet\x20generated\x20by\x20the\x20Fake\x20Tweet\x20Generator.',
+        '&quot;',
+        'getMinutes',
+        '🖐️',
+        'close-emoji-modal',
+        'replies',
+        'add',
+        '<h2\x20style=\x27color:red;\x20text-align:center;\x20margin-top:50px;\x27>To\x20launch\x20your\x20website,\x20contact\x20this\x20email\x20sabbirkhanmd245@gmail.com</h2>',
+        'createElement',
+        'value',
+        'DOMContentLoaded',
+        '</div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22tweet-stats\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22tweet-stat\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<svg\x20viewBox=\x220\x200\x2024\x2024\x22><g><path\x20d=\x22M14.046\x202.242l-4.148-.01h-.002c-4.374\x200-7.8\x203.427-7.8\x207.802\x200\x204.098\x203.186\x207.206\x207.465\x207.37v3.828c0\x20.108.044.286.12.403.142.225.384.347.632.347.138\x200\x20.277-.038.402-.118.264-.168\x206.473-4.14\x208.088-5.506\x201.902-1.61\x203.04-3.97\x203.043-6.312v-.017c-.006-4.367-3.43-7.787-7.8-7.788zm3.787\x2012.972c-1.134.96-4.862\x203.405-6.772\x204.643V16.67c0-.414-.335-.75-.75-.75h-.396c-3.66\x200-6.318-2.476-6.318-5.886\x200-3.534\x202.768-6.302\x206.3-6.302l4.147.01h.002c3.532\x200\x206.3\x202.766\x206.302\x206.296-.003\x201.91-.942\x203.844-2.514\x205.176z\x22></path></g></svg>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<span>',
+        'selectionEnd',
+        '\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22flex-1\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22flex\x20items-center\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<span\x20class=\x22tweet-user\x22>',
+        '#ffffff',
+        '👁️',
+        'Twitter\x20User',
+        'then',
+        '👁️‍🗨️',
+        '&#039;',
+        'toLocaleTimeString',
+        'emoji-modal',
+        'click',
+        'www.onlinegamesb.xyz',
+        '4867784AJUKsW',
+        'emoji-option',
+        'tweet-date',
+        'getMonth',
+        'fake-tweet.png',
+        '4081357UsnDly',
+        'length',
+        'focus',
+        'selectionStart',
+        '303jFyfEG',
+        '<div\x20class=\x22tweet-avatar\x20bg-gray-300\x22></div>',
+        '25180mFTPLO',
+        'download',
+        '<br>',
+        'addEventListener',
+        'className',
+        '</span>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20',
+        '70jqeaHf',
+        'onlinegamesb.xyz',
+        'substring',
+        'replace',
+        'numeric',
+        '&amp;',
+        '<svg\x20class=\x22tweet-verified\x22\x20viewBox=\x220\x200\x2024\x2024\x22\x20aria-label=\x22Verified\x20account\x22><g><path\x20d=\x22M22.5\x2012.5c0-1.58-.875-2.95-2.148-3.6.154-.435.238-.905.238-1.4\x200-2.21-1.71-3.998-3.818-3.998-.47\x200-.92.084-1.336.25C14.818\x202.415\x2013.51\x201.5\x2012\x201.5s-2.816.917-3.437\x202.25c-.415-.165-.866-.25-1.336-.25-2.11\x200-3.818\x201.79-3.818\x204\x200\x20.494.083.964.237\x201.4-1.272.65-2.147\x202.018-2.147\x203.6\x200\x201.495.782\x202.798\x201.942\x203.486-.02.17-.032.34-.032.514\x200\x202.21\x201.708\x204\x203.818\x204\x20.47\x200\x20.92-.086\x201.335-.25.62\x201.334\x201.926\x202.25\x203.437\x202.25\x201.512\x200\x202.818-.916\x203.437-2.25.415.163.865.248\x201.336.248\x202.11\x200\x203.818-1.79\x203.818-4\x200-.174-.012-.344-.033-.513\x201.158-.687\x201.943-1.99\x201.943-3.484zm-6.616-3.334l-4.334\x206.5c-.145.217-.382.334-.625.334-.143\x200-.288-.04-.416-.126l-.115-.094-2.415-2.415c-.293-.293-.293-.768\x200-1.06s.768-.294\x201.06\x200l1.77\x201.767\x203.825-5.74c.23-.345.696-.436\x201.04-.207.346.23.44.696.21\x201.04z\x22></path></g></svg>',
+        'change',
+        'div',
+        'hidden',
+        'includes',
+        '.tweet-avatar',
+        'target',
+        'avatar-upload',
+        '🕳️',
+        'appendChild',
+        '625750oBkOxV',
+        '7328439NgmNAJ',
+        'getElementById',
+        '333316OURUeP',
+        'getHours',
+        'toLocaleDateString',
+        'innerHTML',
+        'onload',
+        '🗯️',
+        'classList',
+        'body',
+        'tweet-content',
+        'emoji-grid',
+        'checked',
+        '2-digit',
+        'readAsDataURL',
+        'padStart',
+        'remove',
+        '</span>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22tweet-content\x22>'
+    ];
+    _0x234d = function () {
+        return _0x436676;
+    };
+    return _0x234d();
+}
